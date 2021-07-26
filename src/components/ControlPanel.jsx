b269ca4de18cec40e8c81e38cd093d79a54df299
hohin728 <45099778+hohin728@users.noreply.github.com>
2021-07-26 18:45:35 +0800

    Add material ui icons and style the Control Panel

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -1,5 +1,4 @@
 import React from "react"
-import styles from "../styles/ControlPanel.module.scss"
 import { useSelector, useDispatch } from "react-redux"
 import {
 	timerStatusUpdated,
@@ -17,9 +16,32 @@ import {
 } from "../features/timers/timersSlice"
 import TimerStatus from "../features/timers/TimerStatus"
 
-import { Button, Box } from "@material-ui/core"
+import {
+	Button,
+	IconButton,
+	Box,
+	Paper,
+	InputLabel,
+	MenuItem,
+	FormControl,
+	makeStyles,
+	TextField,
+} from "@material-ui/core"
+import VolumeOffRoundedIcon from "@material-ui/icons/VolumeOffRounded"
+import VolumeUpRoundedIcon from "@material-ui/icons/VolumeUpRounded"
+import { Select } from "@material-ui/core"
+
+const useStyles = makeStyles((theme) => ({
+	formControl: {
+		width: 150,
+	},
+	toolbar: {
+		marginTop: 16,
+	},
+}))
 
 const ControlPanel = () => {
+	const classes = useStyles()
 	const dispatch = useDispatch()
 
 	const delay = useSelector(selectTimerDelay)
@@ -60,58 +82,65 @@ const ControlPanel = () => {
 	}
 
 	return (
-		<div className={styles.controlPanel}>
-			<div className={styles.buttonsSection}>
-				<Box m={1}>
-					<Button
-						onClick={handleStart}
-						variant="contained"
-						color="primary"
-						size="large"
-					>
-						{timerStatus === TimerStatus.RUNNING ? "Pause" : "Start"}
-					</Button>
+		<Paper elevation={3}>
+			<Box p={3}>
+				<Box display="flex" justifyContent="center">
+					<Box m={1}>
+						<Button
+							onClick={handleStart}
+							variant="contained"
+							color="primary"
+							size="large"
+						>
+							{timerStatus === TimerStatus.RUNNING ? "Pause" : "Start"}
+						</Button>
+					</Box>
+					<Box m={1}>
+						<Button
+							onClick={handleReset}
+							variant="contained"
+							color="secondary"
+							size="large"
+						>
+							Reset
+						</Button>
+					</Box>
 				</Box>
-				<Box m={1}>
-					<Button
-						onClick={handleReset}
-						variant="contained"
-						color="secondary"
-						size="large"
-					>
-						Reset
-					</Button>
+
+				<Box
+					display="flex"
+					justifyContent="space-around"
+					className={classes.toolbar}
+				>
+					<IconButton onClick={handleToggleMusicMuted}>
+						{isMuted ? <VolumeOffRoundedIcon /> : <VolumeUpRoundedIcon />}
+					</IconButton>
+
+					<FormControl className={classes.formControl}>
+						<InputLabel>Frequency (ms)</InputLabel>
+						<Select
+							id="updateFreq"
+							value={delay}
+							onChange={(e) => handleDelayChange(e)}
+						>
+							<MenuItem value={10}>10</MenuItem>
+							<MenuItem value={100}>100</MenuItem>
+							<MenuItem value={1000}>1000</MenuItem>
+						</Select>
+					</FormControl>
+
+					<FormControl className={classes.formControl}>
+						<TextField
+							id="loop"
+							label="No. of rounds"
+							type="number"
+							value={loopTotal}
+							onChange={(e) => handleLoopChanged(e)}
+						/>
+					</FormControl>
 				</Box>
-			</div>
-
-			<div className={`${styles.toolbar}`}>
-				<button onClick={handleToggleMusicMuted}>
-					{isMuted ? "Unmute" : "Mute"}
-				</button>
-
-				<div>
-					<label htmlFor="updateFreq">Delay: </label>
-					<input
-						type="number"
-						id="updateFreq"
-						className={`${styles.input}`}
-						value={delay ?? 0}
-						onChange={(e) => handleDelayChange(e)}
-					/>
-				</div>
-
-				<div>
-					<label htmlFor="loop">Loop: </label>
-					<input
-						type="number"
-						id="loop"
-						className={`${styles.input}`}
-						value={loopTotal}
-						onChange={(e) => handleLoopChanged(e)}
-					/>
-				</div>
-			</div>
-		</div>
+			</Box>
+		</Paper>
 	)
 }
 
