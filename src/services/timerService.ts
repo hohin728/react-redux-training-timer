5411f8b2e76c0759d0b237db444ee21c2c896cd7
Gordon Tang <hohin.tang@gmail.com>
2021-08-19 14:24:33 +0800

    Install typescript and add it to timerService

--- src/services/timerService.ts
+++ src/services/timerService.ts
@@ -1,11 +1,15 @@
 import { isInteger } from "lodash"
 import { v4 as uuidv4 } from "uuid"
+import TimerType from "../types/Timer"
 
 // default time when user's preference is not found
 export const defaultMinuteIfNotSaved = 1
 export const defaultSecondIfNotSaved = 30
 
-export const calcTimerRemainTime = (params) => {
+export const calcTimerRemainTime = (params: {
+	minute: number
+	second: number
+}): number => {
 	const { minute, second } = params
 
 	if (isInteger(minute) && isInteger(second)) {
@@ -14,7 +18,13 @@ export const calcTimerRemainTime = (params) => {
 	return 0
 }
 
-export const createTimer = (params) => {
+export const createTimer = (params: {
+	timerId: string
+	label: string
+	minute: number
+	second: number
+	music: string
+}): TimerType => {
 	const id = params && params.timerId ? params.timerId : uuidv4()
 	const timer = {
 		id: id,
@@ -39,7 +49,13 @@ export const createTimer = (params) => {
 	return timer
 }
 
-export const convertTimeFormatForDisplay = (convertMillisec) => {
+export const convertTimeFormatForDisplay = (
+	convertMillisec: number
+): {
+	millisec: number
+	second: number
+	minute: number
+} => {
 	if (!isInteger(convertMillisec) || convertMillisec <= 0) {
 		return {
 			millisec: 0,
@@ -59,22 +75,34 @@ export const convertTimeFormatForDisplay = (convertMillisec) => {
 	}
 }
 
-export const getInvalidTimeInputMessage = ({ value, timeUnit }) => {
+export const getInvalidTimeInputMessage = ({
+	value,
+	timeUnit,
+}: {
+	value: number
+	timeUnit: "minute" | "second"
+}): string => {
 	if (timeUnit !== "minute" && timeUnit !== "second") {
 		// console.log("Time unit must be either 'minute' or 'second'")
 		return "Time unit must be either 'minute' or 'second'"
 	}
-	if (!Number.isInteger(parseInt(value))) {
+	if (!Number.isInteger(value)) {
 		// console.log("Time value must be an integer")
 		return "Time value must be an integer"
 	}
-	if (parseInt(value) < 0 || parseInt(value) > 59) {
+	if (value < 0 || value > 59) {
 		// console.log("Time value must be between 0 and 59")
 		return "Time value must be between 0 and 59"
 	}
 	return ""
 }
 
-export const isValidTimeInput = ({ value, timeUnit }) => {
+export const isValidTimeInput = ({
+	value,
+	timeUnit,
+}: {
+	value: number
+	timeUnit: "minute" | "second"
+}): boolean => {
 	return getInvalidTimeInputMessage({ value, timeUnit }) === ""
 }
---
+++ src/types/Timer.ts
@@ -0,0 +1,10 @@
+type TimerType = {
+	id: string
+	label: string
+	minute: number
+	second: number
+	remainTime: number
+	music: string
+}
+
+export default TimerType
